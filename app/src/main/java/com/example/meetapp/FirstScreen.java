5622b365e5e80607262d374adf20077de3bfc59a
Chen <chen.benyashar@mail.huji.ac.il>
2018-12-24 17:39:51 +0200

    visual change of "new group" popup. Fits general design and color schemas better now

--- app/src/main/java/com/example/meetapp/FirstScreen.java
+++ app/src/main/java/com/example/meetapp/FirstScreen.java
@@ -2,11 +2,12 @@ package com.example.meetapp;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -54,7 +55,7 @@ public class FirstScreen extends AppCompatActivity {
 
     private void handleCreateNewGroup()
     {
-        TextView createGroup;
+        Button createGroup;
         createGroup = newGroupDialog.findViewById(R.id.CreateGroupBtn);
         createGroup.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/meetapp/GroupAdapter.java
+++ app/src/main/java/com/example/meetapp/GroupAdapter.java
@@ -43,7 +43,7 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.ViewHolder>
             public void onClick(View v) {
                 Intent goToGroupScreen = new Intent(context, GroupActivity.class);
                 goToGroupScreen.putExtra("groupName", newGroup.getName());
-                goToGroupScreen.putExtra("groupMembers", newGroup.getMembersString());
+                goToGroupScreen.putExtra("groupMembers", newGroup.getMembers());
                 context.startActivity(goToGroupScreen);
             }
         });
