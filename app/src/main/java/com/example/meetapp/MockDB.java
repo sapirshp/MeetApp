6787f10ada3633a69ad00fd2de0507e3cd4134b7
sapirshp <sprir3@gmailcom>
2019-01-07 18:44:42 +0200

    fixed MockDB bug - when clickedOn slotSelection Number is now increased just by one

--- app/src/main/java/com/example/meetapp/MockDB.java
+++ app/src/main/java/com/example/meetapp/MockDB.java
@@ -1,63 +1,19 @@
 package com.example.meetapp;
 
-import android.content.Context;
-import android.view.View;
-import android.widget.Button;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
-public class MockDB {
+class MockDB {
 
-    private static HashMap<TimeSlot, Integer> mockSlotSelections = new HashMap<>();
-    private static HashMap<Integer, String> buttonsIdForMockDB = new HashMap<>();
-    private static ArrayList<TimeSlot> calendarSlots = new ArrayList<>();
-
-    private static int getHourIndex(String dayPart)
-    {
-        switch(dayPart) {
-            case "Morning":
-                return 0;
-            case "Afternoon":
-                return 1;
-            default:
-                return 2;
-        }
-    }
-
-    private static void createMockSelections(View v, Context context, Map<String, String> datesToDisplay, int membersNum) {
-        for (int i = 0; i < 7; i++) {
-            int morningBtnId = context.getResources().getIdentifier("d" + i + "m", "id", context.getPackageName());
-            buttonsIdForMockDB.put(morningBtnId, "Morning");
-            int afternoonBtnId = context.getResources().getIdentifier("d" + i + "a", "id", context.getPackageName());
-            buttonsIdForMockDB.put(afternoonBtnId, "Afternoon");
-            int eveningBtnId = context.getResources().getIdentifier("d" + i + "e", "id", context.getPackageName());
-            buttonsIdForMockDB.put(eveningBtnId, "Evening");
-        }
-        for (int id : buttonsIdForMockDB.keySet()) {
-            final Button timeSlotButton = v.findViewById(id);
-            int dateIndex = Integer.valueOf(timeSlotButton.getTag().toString());
-            String date = datesToDisplay.get(datesToDisplay.keySet().toArray()[dateIndex]);
-            String hour = buttonsIdForMockDB.get(id);
-            int hourIndex = getHourIndex(hour);
-            int slotIndex = (3 * dateIndex) + hourIndex;
-            final TimeSlot timeSlot = new TimeSlot(timeSlotButton, date, hour, slotIndex);
-            calendarSlots.add(timeSlot);
-        }
-        for (TimeSlot ts : calendarSlots){
+    static void createMockSelections(HashMap<TimeSlot, Integer> mockSlotSelections, int membersNum) {
+        for (TimeSlot ts : mockSlotSelections.keySet()){
             Random rand = new Random();
-            int randomNum = rand.nextInt((membersNum - 0) + 1) + 0;
+            int randomNum = rand.nextInt((membersNum-1 - 0) + 1) + 0;
             mockSlotSelections.put(ts, randomNum);
         }
-    }
-
-    static HashMap<TimeSlot, Integer> getMockSlotSelections(View v, Context context, Map<String, String> datesToDisplay, int membersNum){
-        createMockSelections(v,context,datesToDisplay, membersNum);
-        return mockSlotSelections;
     }
 
     static ArrayList<Group> buildMockGroups(String userName, ArrayList<Group> groups){
