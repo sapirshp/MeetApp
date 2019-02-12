fc7f9ed9b784e92f2b2f516a31d4dd194fed6d65
Chen <chen.benyashar@mail.huji.ac.il>
2019-02-12 23:10:14 +0200

    allign and spacing

--- app/src/main/java/com/example/meetapp/MenuHandler.java
+++ app/src/main/java/com/example/meetapp/MenuHandler.java
@@ -320,7 +320,6 @@ class MenuHandler {
         userInput.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after){}
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (s.length() == 0){
